93c5e2d02e83bc14da1e809623699b1debe09bf5
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2024-06-16 20:08:39 -0400

    made an update to the movement again
    tried a different tutorial bc the previous one wasn't built well. redid the movement, i think its better but I made a mistake somewhere so I need to fix that tomorrow probably

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,61 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //used code from https://www.youtube.com/@davegamedevelopment
+    //very good and understandable code for movement and camera
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObject;
+    public Rigidbody rb;
+    public float rot_speed = 7.0f;
 
     public float player_speed = 5f;
-    public float player_deceleration = 0.9f;
-    public float jump_height = 20f;
+    public float groundDrag;
 
-    public GroundDetection GD; //Script in the player rigidbody that returns if the player is grounded
-    public GameObject player;
-    public GameObject cam;
-    Rigidbody rb;
+    [Header("Ground Detection")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool isGrounded;
 
-    private float mouse_X;
-    private float mouse_Y;
-    public float cam_speed = 500.0f;
-
-    Vector3 movement_Direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = player.GetComponentInChildren<Rigidbody>();
+        rb = player.GetComponentInParent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        //ground check (said zestily)
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + .2f, whatIsGround);
 
-        //gets the input direction from the predefined set buttons in project settings and sets them as a vector
-        movement_Direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * player_speed;// a value between -1 and 1 for the two axis
-        movement_Direction.y = rb.velocity.y;
-        rb.velocity = movement_Direction;//setting the rb velocity to the movement direction * the speed of the player
+        //drag controller
+        if (isGrounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
 
-        rb.rotation = Quaternion.Euler(0f, rb.rotation.eulerAngles.y, 0f);//this quaternion thing somehow prevents the player from tipping over lol no one rlly explained it to me
-        Debug.Log("The player is grounded: " + GD.getGrounded());
+        //rotate orientation
+        Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDirection.normalized;
 
-        //jump
-        //TODO need to add jump cool down 
-        if(Input.GetAxis("Jump") >0 && GD.getGrounded())
+        //rotate player object
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputDirection = orientation.forward *verticalInput + orientation.right * horizontalInput;
+
+        if(inputDirection != Vector3.zero)
         {
-            rb.AddForce(0f, jump_height,0f);
+            playerObject.forward = Vector3.Slerp(playerObject.forward, inputDirection.normalized, Time.deltaTime * rot_speed);
         }
 
+    }
 
-        //CAMERA MOVEMENT
-        //cam.transform.position = rb.position.x;
+    private void FixedUpdate()
+    {
+        //movePlayer();
+        speedControl();
+    }
 
+    private void movePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");//POTENTIAL AREA OF LAG I THINK
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveDirection = orientation.forward *verticalInput + orientation.right * horizontalInput;
 
-        //if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
-        mouse_X = Input.GetAxis("Mouse X") * Time.deltaTime * cam_speed;
-        mouse_Y = Input.GetAxis("Mouse Y") * Time.deltaTime * cam_speed;
-        //transform.Rotate(Vector3.right, mouse_Y);
-        transform.Rotate(Vector3.up, mouse_X, Space.World);
+        rb.AddForce(moveDirection.normalized *  player_speed * 10f , ForceMode.Force);
+    }
+
+    void speedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVelocity.magnitude > player_speed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized*player_speed; 
+            rb.velocity = new Vector3(limitedVelocity.x ,rb.velocity.y, limitedVelocity.z);
+        }
     }
 }
